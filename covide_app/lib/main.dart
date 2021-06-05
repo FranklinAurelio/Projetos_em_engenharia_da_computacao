90eb3ea13e6b03c3349d907fa3ae5bc6d4ed9744
Franklin Aurelio <franklin.carvalho@unifesp.br>
2021-06-05 11:00:14 -0300

    base do app

---
+++ covide_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:covide_app/screens/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Covid app',
+      home: MyHomePage(),
+    );
+  }
+}
+
---
+++ covide_app/lib/screens/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+        child: Container(
+        ),
+      ),
+    );
+  }
+}
